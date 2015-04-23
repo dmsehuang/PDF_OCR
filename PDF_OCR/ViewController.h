f3059a125e9a924628a0a08ca627c467b275a3d2
huijinghuang <dmsehuang@huijinghuangs-MacBook-Pro.local>
2015-04-22 19:12:54 -0700

    Initial Commit

---
+++ PDF_OCR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDF_OCR
+//
+//  Created by huijinghuang on 4/22/15.
+//  Copyright (c) 2015 huijinghuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PDF_OCR/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PDF_OCR
+//
+//  Created by huijinghuang on 4/22/15.
+//  Copyright (c) 2015 huijinghuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
